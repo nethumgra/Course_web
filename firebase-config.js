03f78446d71d4fa4d92f0f50faa655de72276253
nethumgra <teddygraphics84@gmail.com>
2026-02-14 13:02:51 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAVu0_PvOOA_1D1Dk0La4Gy-X_dlrq5n4s",
-    authDomain: "mindinu-vege.firebaseapp.com",
-    projectId: "mindinu-vege",
-    storageBucket: "mindinu-vege.appspot.com",
-    messagingSenderId: "122155864706",
-    appId: "1:122155864706:web:42f6c6aff06b3aeb779a9b",
-    measurementId: "G-8X3DYYXF2K"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyDFZrzFCsqVq4FUKB1onpkHR70cYh3ZCPA",
+    authDomain: "waultdot-design.firebaseapp.com",
+    projectId: "waultdot-design",
+    storageBucket: "waultdot-design.firebasestorage.app",
+    messagingSenderId: "508816172974",
+    appId: "1:508816172974:web:6482af561fb2daffd98a3f",
+    measurementId: "G-6G89N8B72Q"
+  };
+
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
